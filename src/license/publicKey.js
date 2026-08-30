71b440c5a5a10402b2fc0892aa0d876c23b025e0
gosha01071997 <goshakondratev777@gmail.com>
2026-08-30 17:55:33 +0300

    Update production Ed25519 license public key

--- src/license/publicKey.js
+++ src/license/publicKey.js
@@ -1,2 +1,2 @@
 // Ed25519 public key (raw, base64url). The corresponding private key is not part of the app.
-export const LICENSE_PUBLIC_KEY = "v3H-5bmPUd2Kb0SVqZ55LE3BWAtqyC3Q_EKdL4zyn7g";
+export const LICENSE_PUBLIC_KEY = "el0V6qVwgxLm1azSG2uB_6_OuBafM9BrhHfNWXUMxHM";
